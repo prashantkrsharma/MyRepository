604e7d72b0ad04aca9ad7287d1dffe3417e3dc47
Prashant <prashantkrsharma@yahoo.com>
2023-02-12 23:56:53 -0800

    initial commit

---
+++ Randomize-ASP.NET-main/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Randomize.Models;
+
+namespace Randomize.Controllers
+{
+    public class MainController : Controller
+    {
+        //get
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Main(Digits digit)
+        {
+            if (digit.numberTwo == digit.numberOne)
+                ModelState.AddModelError("numberOne", "The numbers are equal");
+            else if (digit.numberTwo < digit.numberOne)
+                ModelState.AddModelError("numberOne", "Incorrectly entered numbers in the range");
+
+            if (ModelState.IsValid)
+            {
+                Random rand = new Random();
+                digit.result = rand.Next(digit.numberOne, digit.numberTwo+1);
+            }
+
+            return View(digit);
+        }
+    }
+}
---
+++ Randomize-ASP.NET-main/Models/Digits.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Randomize.Models
+{
+    public class Digits
+    {
+        [Required (ErrorMessage = "Enter a number in the range from -10000 to 10000")]
+        public int numberOne { get; set; }
+
+        [Required (ErrorMessage = "Enter a number in the range from -10000 to 10000")]
+        public int numberTwo { get; set; }
+
+        public int result { get; set; }
+    }
+}
